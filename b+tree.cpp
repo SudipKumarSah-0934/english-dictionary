1515254e75a8aeac4e4561357abcdaaeb9048133
SudipKumarSah-0934 <46530574+SudipKumarSah-0934@users.noreply.github.com>
2019-10-23 10:00:40 +0600

    Add files via upload

--- b+tree.cpp
+++ b+tree.cpp
@@ -1,25 +1,21 @@
-#include<iostream>
-#include<cmath>
-#include<algorithm>
-#include<fstream>
+#include<bits/stdc++.h>
 using namespace std;
-struct tree{
+struct Btree{
     string **data = new string*[10];
-    int i;
-    tree()
+    Btree()
     {
-        for(i=0; i<10; i++)
+        for(int i=0; i<10; i++)
         {
             data[i] = new string[2];
         }
     }
-    tree* link[25] = {NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL};
-    tree* prev = NULL;
+    Btree* link[11] = {NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL};
+    Btree* prev = NULL;
     int number = 0;
 };
-tree* root = NULL;
+Btree* root = NULL;
 ofstream ofile;
-tree* findBlock(string a, tree* t1){
+Btree* findBlock(string a, Btree* t1){
     if(!(*t1).link[0]){
         return t1;
     }
@@ -29,7 +25,7 @@ tree* findBlock(string a, tree* t1){
             return findBlock(a, t1);
         }
         else if((*t1).link[i+1] == NULL){
-            tree *t3 = new tree();
+            Btree *t3 = new Btree();
             (*t3).prev = t1;
             (*t1).link[i+1] = t3;
             return t3;
@@ -51,7 +47,7 @@ void bubbleSort(string **arr, int n){
         }
 }
 
-void link_leaf(tree *temp){
+void link_leaf(Btree *temp){
     if(!temp || !(*temp).link[0]){
         return;
     }
@@ -60,30 +56,30 @@ void link_leaf(tree *temp){
     }
     if(temp == root){
         for(int i=0; i<(*temp).number; i++){
-            tree *t1 = (*temp).link[i];
+            Btree *t1 = (*temp).link[i];
             (*t1).link[(*t1).number] = (*temp).link[i+1];
         }
     }
     else{
-        tree *t1 = (*temp).prev;
+        Btree *t1 = (*temp).prev;
         for(int i=0; i<(*t1).number; i++){
-            tree *t2 = (*t1).link[i];
-            tree *t3 = (*t2).link[(*t2).number];
+            Btree *t2 = (*t1).link[i];
+            Btree *t3 = (*t2).link[(*t2).number];
             (*t3).link[(*t3).number] = (*(*t1).link[i+1]).link[0];
             for(int i=0; i<(*t2).number; i++){
-                tree *t4 = (*t2).link[i];
+                Btree *t4 = (*t2).link[i];
                 (*t4).link[(*t4).number] = (*t2).link[i+1];
             }
         }
-        tree *t2 = (*t1).link[(*t1).number];
+        Btree *t2 = (*t1).link[(*t1).number];
         for(int i=0; i<(*t2).number; i++){
-                tree *t4 = (*t2).link[i];
+                Btree *t4 = (*t2).link[i];
                 (*t4).link[(*t4).number] = (*t2).link[i+1];
             }
     }
 }
 
-void insert_in_tree(string a, string b, tree* t1, tree* c1, tree* c2){
+void InsertInBtree_in_Btree(string a, string b, Btree* t1, Btree* c1, Btree* c2){
     if((*t1).number < 10){
         (*t1).data[(*t1).number][0] = a;
         (*t1).data[(*t1).number][1] = b;
@@ -141,8 +137,8 @@ void insert_in_tree(string a, string b, tree* t1, tree* c1, tree* c2){
             arr[i][1] = (*t1).data[i - 1][1];
         }
         bubbleSort(arr, 11);
-        tree *tn1 = new tree();
-        tree *tn2 = new tree();
+        Btree *tn1 = new Btree();
+        Btree *tn2 = new Btree();
         for(int i=0; i<6; i++){
             (*tn1).data[i][0] = arr[i][0];
             (*tn1).data[i][1] = arr[i][1];
@@ -153,22 +149,22 @@ void insert_in_tree(string a, string b, tree* t1, tree* c1, tree* c2){
         }
         (*tn1).number = 6;
         (*tn2).number = 5;
-        tree *temp;
+        Btree *temp;
         if(t1 == root){
-            tree *tn3 = new tree();
+            Btree *tn3 = new Btree();
             (*tn3).number = 0;
             (*tn3).prev = NULL;
             root = tn3;
             temp = root;
             (*tn1).prev = root;
             (*tn2).prev = root;
-            insert_in_tree((*tn2).data[0][0], (*tn2).data[0][1], tn3, tn1, tn2);
+            InsertInBtree_in_Btree((*tn2).data[0][0], (*tn2).data[0][1], tn3, tn1, tn2);
         }
         else{
             temp = (*t1).prev;
             (*tn1).prev = temp;
             (*tn2).prev = temp;
-            insert_in_tree((*tn2).data[0][0], (*tn2).data[0][1], temp, tn1, tn2);
+            InsertInBtree_in_Btree((*tn2).data[0][0], (*tn2).data[0][1], temp, tn1, tn2);
         }
         link_leaf(root);
     }
@@ -184,8 +180,8 @@ void insert_in_tree(string a, string b, tree* t1, tree* c1, tree* c2){
             arr[i][1] = (*t1).data[i - 1][1];
         }
         bubbleSort(arr, 11);
-        tree *tn1 = new tree();
-        tree *tn2 = new tree();
+        Btree *tn1 = new Btree();
+        Btree *tn2 = new Btree();
         for(int i=0; i<5; i++){
             (*tn1).data[i][0] = arr[i][0];
             (*tn1).data[i][1] = arr[i][1];
@@ -196,9 +192,9 @@ void insert_in_tree(string a, string b, tree* t1, tree* c1, tree* c2){
         }
         (*tn1).number = 5;
         (*tn2).number = 5;
-        tree *temp;
+        Btree *temp;
         if(t1 == root){
-            temp = new tree();
+            temp = new Btree();
             (*temp).data[0][0] = arr[5][0];
             (*temp).data[0][1] = arr[5][1];
             (*temp).number = 1;
@@ -212,7 +208,7 @@ void insert_in_tree(string a, string b, tree* t1, tree* c1, tree* c2){
             temp = (*t1).prev;
             (*tn1).prev = temp;
             (*tn2).prev = temp;
-            insert_in_tree(arr[5][0], arr[5][1], temp, tn1, tn2);
+            InsertInBtree_in_Btree(arr[5][0], arr[5][1], temp, tn1, tn2);
         }
         for(int i=0; i<5; i++){
             for(int j=0; j<6; j++){
@@ -272,39 +268,45 @@ void insert_in_tree(string a, string b, tree* t1, tree* c1, tree* c2){
 }
 
 void leafTraverse(){
-    tree *traversal = root;
+    Btree *traversal = root;
     while((*traversal).link[0] != NULL){
         traversal = (*traversal).link[0];
     }
+    int count1 =0;
     while(traversal != NULL){
+
         for(int i=0; i<(*traversal).number; i++){
-            //cout<<(*traversal).data[i][0]<<"  ";
+            cout<<(*traversal).data[i][0]<<"  ";
+    count1++;
         }
+
         traversal = (*traversal).link[(*traversal).number];
     }
+    cout<<endl<<"LEAF TRAVERSAL"<<count1<<endl;
 }
 
-void Insert(string a, string b){
+void InsertInBtree(string a, string b){
     if(root ==NULL){
-        tree* t1 = new tree();
+        Btree* t1 = new Btree();
         root = t1;
         (*t1).data[0][0] = a;
         (*t1).data[0][1] = b;
         (*t1).number += 1;
         return;
     }
-    tree *t2 = root;
+    Btree *t2 = root;
     t2 = findBlock(a, t2);
-    insert_in_tree(a, b, t2, NULL, NULL);
+    InsertInBtree_in_Btree(a, b, t2, NULL, NULL);
 }
 
-void traverse(tree *t){
+void traverse(Btree *t){
     if(t == NULL){
         return;
     }
+
     else if((*t).link[0] == NULL){
         for(int i=0; i<(*t).number; i++){
-            cout<<(*t).data[i][0]<<"   ";
+            //cout<<(*t).data[i][0]<<"   ";
         }
         return;
     }
@@ -316,7 +318,7 @@ void traverse(tree *t){
     return;
 }
 
-int searchValue(string a, tree* temp){
+int searchword(string a, Btree* temp){
     if(temp == NULL){
         cout<<"null"<<endl;
         return -1;
@@ -324,8 +326,9 @@ int searchValue(string a, tree* temp){
     if((*temp).link[0] == NULL){
         for(int i=0; i<(*temp).number; i++){
             if((*temp).data[i][0] == a){
-                cout<<"found\n"<<(*temp).data[i][0]<<"--"<<(*temp).data[i][1]<<endl;
-                ofile<<(*temp).data[i][1]<<endl;
+               // cout<<"found\n"<<(*temp).data[i][0]<<"--"<<(*temp).data[i][1]<<endl;
+               cout<<"found";
+                ofile<<(*temp).data[i][0]<<"--"<<(*temp).data[i][1]<<endl;
                 return 1;
             }
         }
@@ -333,16 +336,16 @@ int searchValue(string a, tree* temp){
         return -1;
     }
     else if (a < (*temp).data[0][0]){
-        searchValue(a, (*temp).link[0]);
+        searchword(a, (*temp).link[0]);
     }
     else if(a >= (*temp).data[(*temp).number-1][0]){
-        searchValue(a, (*temp).link[(*temp).number]);
+        searchword(a, (*temp).link[(*temp).number]);
     }
     else{
         for(int i=1; i<(*temp).number; i++){
             if(a < (*temp).data[i][0]){
                 //cout<<1<<"  "<<(*temp).data[i][0]<<endl;
-                int x = searchValue(a, (*temp).link[i]);
+                int x = searchword(a, (*temp).link[i]);
                 if(x == 1){
                     return 1;
                 }
@@ -374,15 +377,16 @@ int main(){
         {
         	str2+=input[j];
         }
-        Insert(str1, str2);
+        InsertInBtree(str1, str2);
     }
     leafTraverse();
-    tree *print = root;
+    Btree *print = root;
     traverse(print);
-    cout<<endl<<"enter word"<<endl;
+    cout<<"enter word"<<endl;
     while(cin>>str && str!=""){
-        tree *temp = root;
-        searchValue(str, temp);
+        Btree *temp = root;
+        searchword(str, temp);
+        cout<<endl<<"enter another word"<<endl;
     }
 }
 
